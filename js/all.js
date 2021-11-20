c201d76b65ba4d46f1a84a1114dae6f87d31d246
joyun25 <88151037+joyun25@users.noreply.github.com>
2021-11-20 19:25:52 +0800

    img can't show on github pages so I adjust the wrong img position in js

--- js/all.js
+++ js/all.js
@@ -3,51 +3,51 @@ let foodList = [
     {
         "item":"Eggs",
         "price":"60 NTW / 10 eggs",
-        "image":"../images/eggs.jpg"
+        "image":"images/eggs.jpg"
     },{
         "item":"Apples",
         "price":"150 NTW / 10 apples",
-        "image":"../images/apples.jpg"
+        "image":"images/apples.jpg"
     },{
         "item":"Bananas",
         "price":"15 NTW / 600 g",
-        "image":"../images/bananas.jpg"
+        "image":"images/bananas.jpg"
     },{
         "item":"Grapes",
         "price":"40 NTW / 600 g",
-        "image":"../images/grapes.jpg"
+        "image":"images/grapes.jpg"
     },{
         "item":"Beef",
         "price":"200 NTW / 600 g",
-        "image":"../images/beef.jpg"
+        "image":"images/beef.jpg"
     },{
         "item":"Milk",
         "price":"90 NTW / 1 ml",
-        "image":"../images/milk.jpg"
+        "image":"images/milk.jpg"
     },{
         "item":"Chicken",
         "price":"120 NTW / 600 g",
-        "image":"../images/chicken.jpg"
+        "image":"images/chicken.jpg"
     },{
         "item":"Cheese",
         "price":"300 NTW / 3 kg",
-        "image":"../images/cheese.jpg"
+        "image":"images/cheese.jpg"
     },{
         "item":"Pineapples",
         "price":"25 NTW / 600 g",
-        "image":"../images/pineapples.jpg"
+        "image":"images/pineapples.jpg"
     },{
         "item":"Carrots",
         "price":"10 NTW / 1 carrot",
-        "image":"../images/carrots.jpg"
+        "image":"images/carrots.jpg"
     },{
         "item":"Mushrooms",
         "price":"30 NTW / 600 g",
-        "image":"../images/mushrooms.jpg"
+        "image":"images/mushrooms.jpg"
     },{
         "item":"Eggplants",
         "price":"50 NTW / 3 eggplants",
-        "image":"../images/eggplants.jpg"
+        "image":"images/eggplants.jpg"
     },
 ];
 let str = "";
